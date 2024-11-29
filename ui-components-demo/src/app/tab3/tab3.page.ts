91011c6b91ca48fe2b6d02d926b768abfde7a02b
AdamAmusa <92576185+AdamAmusa@users.noreply.github.com>
2024-11-29 10:51:25 +0000

    Ion Modal can be triggered open and closed

--- ui-components-demo/src/app/tab3/tab3.page.ts
+++ ui-components-demo/src/app/tab3/tab3.page.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonModal } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonModal, IonInput, IonLabel, IonItem } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-
+import { ModalController } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButtons, IonButton, IonModal],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButtons, IonButton, IonModal, IonInput, IonLabel, IonItem],
 })
 export class Tab3Page {
-  constructor() {}
+  constructor(private modalController: ModalController) {}
+
+  async dismissModal() {
+    const modal = await this.modalController.getTop();
+    if (modal) {
+      modal.dismiss();
+    }
+  }
 }
